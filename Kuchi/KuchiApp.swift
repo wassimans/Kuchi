434350e1a2f393f1f1434a6f5d42f0c6b8ee3383
Wassim MANSOURI <wassim.mansouri@redfabriq.com>
2021-02-13 07:21:46 +0100

    Initial Commit

---
+++ Kuchi/KuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiApp.swift
+//  Kuchi
+//
+//  Created by Wassim Mansouri on 13/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct KuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
